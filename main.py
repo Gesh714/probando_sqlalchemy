172de7442a2242bb20cabcb4e53118d7a718d7a8
Gesh714 <kinegsanchezh@gmail.com>
2023-10-31 22:12:27 +0000

    3

--- main.py
+++ main.py
@@ -5,13 +5,11 @@ from flaskext.mysql import MySQL
 app = Flask(__name__)
 app.config['MYSQL_DATABASE_USER'] = 'root'
 app.config['MYSQL_DATABASE_DB'] = 'probando_mysql'
-mysql = MySQL()
-mysql.init_app(app)
+mysql = MySQL(app)
 
 @app.route('/')
 def mysql():
     return render_template('index.html')
-
 
 if __name__ == '__main__':
     app.run()
