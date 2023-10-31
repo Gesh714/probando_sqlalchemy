59eaadedbf522425c67235a89bf828f9fecbabb8
gesh3020 <kinegsanchezh@gmail.com>
2023-10-31 18:50:45 -0300

    agregando form

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
 from flask import Flask, render_template, url_for, request, redirect
+from flaskext.mysql import MySQL
+
 
 app = Flask(__name__)
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = 'password'
+app.config['MYSQL_DB'] = 'probando_mysql'
+mysql = MySQL(app)
 
 @app.route('/')
 def mysql():
