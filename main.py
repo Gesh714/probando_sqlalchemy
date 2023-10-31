da15a7e6525039edeaf2ad870bc87e3605f3b8bf
gesh3020 <kinegsanchezh@gmail.com>
2023-10-31 19:00:35 -0300

    Update main.py

--- main.py
+++ main.py
@@ -3,18 +3,17 @@ from flaskext.mysql import MySQL
 
 
 app = Flask(__name__)
-app.config['MYSQL_HOST'] = 'localhost'
-app.config['MYSQL_USER'] = 'root@localhost'
-app.config['MYSQL_PASSWORD'] = ''
-app.config['MYSQL_DB'] = 'probando_mysql'
-mysql = MySQL(app)
+app.config['MYSQL_DATABASE_USER'] = 'root'
+app.config['MYSQL_DATABASE_DB'] = 'probando_mysql'
+mysql = MySQL()
+mysql.init_app(app)
 
 @app.route('/')
 def mysql():
     return render_template('index.html')
 
 def new_func(app):
-    app.run(port=3000,debug=True)
+    app.run()
 
 if __name__ == '__main__':
     new_func(app)
